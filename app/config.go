215cb5566e70213ce53604e74c350fbd791ded00
fiffu <fhejehfif@gmail.com>
2022-04-23 00:23:22 +0800

    Unit tests for app/config.go

--- app/config.go
+++ app/config.go
@@ -5,19 +5,13 @@ import (
 )
 
 type Config struct {
-	BotSecret   string                 `mapstructure:"botSecret"`
+	BotSecret   string                 `mapstructure:"botSecret" envvar:"BOT_SECRET"`
 	DatabaseDSN string                 `mapstructure:"database_dsn" envvar:"DATABASE_URL"`
 	Cogs        map[string]interface{} `mapstructure:"cogs"`
-
-	GuildID        string // flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
-	RemoveCommands bool   // flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
 }
 
 func Configure(path string) (*Config, error) {
 	viper.SetConfigFile(path)
-
-	viper.SetEnvPrefix("arisa3") // read env keys prefixed with ARISA3_
-	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
 		return nil, err

cadd8d1c6883dc032c66a172c09c8ead4478f5b7
fiffu <fhejehfif@gmail.com>
2022-05-03 23:22:48 +0800

    Refactor /pokies, add grid size check

--- app/cogs/rng/pokies.go
+++ app/cogs/rng/pokies.go
@@ -5,12 +5,14 @@ import (
 	"strings"
 	"time"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/fiffu/arisa3/app/types"
+	"github.com/fiffu/arisa3/app/utils"
 	"github.com/rs/zerolog/log"
 )
 
 const (
-	PokiesSize = "n"
+	PokiesSize = "grid_size"
 )
 
 func (c *Cog) pokiesCommand() *types.Command {
@@ -19,33 +21,54 @@ func (c *Cog) pokiesCommand() *types.Command {
 		Options(
 			types.NewOption(PokiesSize).
 				Int().
-				Desc("generate result in NxN grid; if not given, result will be a single 3x1 row"),
+				Desc("generate result in N x N grid; if not given, result will be a single row of 3 x 1"),
 		).
 		Handler(c.pokies)
 }
 
 func (c *Cog) pokies(req types.ICommandEvent) error {
-	// Reset the rand seed
-	rand.Seed(time.Now().Unix())
-
-	guildID := "118320695057842183" // req.Interaction().GuildID
-
+	// Query emoji palette
+	guildID := req.Interaction().GuildID
 	emojis, err := req.Session().GuildEmojis(guildID)
-	emojiCount := len(emojis)
 	if err != nil {
 		return err
 	}
 
-	rows, cols := 1, 3
-	log.Info().Msgf("args %v", req.Args())
+	// Parse request, build reply
+	rows, cols, tooBig := parseGrid(req)
+	var reply string
+	if tooBig {
+		reply = "That's just way too much work " + utils.BIRB
+	} else {
+		reply = buildGrid(rows, cols, emojis)
+	}
+
+	resp := types.NewResponse().Content(reply)
+	return req.Respond(resp)
+}
+
+func parseGrid(req types.ICommandEvent) (rows int, cols int, tooBig bool) {
+	rows, cols = 1, 3
 	if size, ok := req.Args().Int(PokiesSize); ok {
+		if size > 9 {
+			tooBig = true
+			return
+		}
 		rows = size
 		cols = size
 	}
+	return
+}
 
+func buildGrid(rows, cols int, emojis []*discordgo.Emoji) string {
 	log.Info().Msgf("Building grid of %d x %d", cols, rows)
 
+	// Reset the rand seed otherwise it will always yield the same result
+	rand.Seed(time.Now().Unix())
+
+	emojiCount := len(emojis)
 	grid := make([]string, rows)
+
 	for y := range grid {
 		row := make([]string, cols)
 		for x := range row {
@@ -55,7 +78,5 @@ func (c *Cog) pokies(req types.ICommandEvent) error {
 		grid[y] = strings.Join(row, " ")
 	}
 
-	reply := strings.Join(grid, "\n")
-	resp := types.NewResponse().Content(reply)
-	return req.Respond(resp)
+	return strings.Join(grid, "\n")
 }
---
+++ app/utils/customemojis.go
@@ -0,0 +1,3 @@
+package utils
+
+const BIRB = "<:birb:841723361289568337>"

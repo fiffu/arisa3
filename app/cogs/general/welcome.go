31f4d0a7bcc95e0d20597238c8fbc9d99c09975e
fiffu <fhejehfif@gmail.com>
2022-06-20 01:02:25 +0800

    Fix invite link, it was missing scope=applications.commands

--- app/cogs/general/welcome.go
+++ app/cogs/general/welcome.go
@@ -1,8 +1,6 @@
 package general
 
 import (
-	"fmt"
-
 	"github.com/fiffu/arisa3/app/engine"
 	"github.com/rs/zerolog/log"
 
@@ -10,12 +8,11 @@ import (
 )
 
 func (c *Cog) welcome(s *dgo.Session, r *dgo.Ready) {
-	invitePerms := dgo.PermissionUseSlashCommands
-	inviteURL := fmt.Sprintf(
-		"https://discordapp.com/oauth2/authorize?client_id=%s&scope=bot&permissions=%d",
-		s.State.User.ID,
-		invitePerms,
-	)
+	rootURL := "https://discordapp.com/oauth2/authorize?"
+	clientID := "client_id=" + s.State.User.ID
+	scope := "scope=applications.commands%20bot"
+	inviteURL := rootURL + clientID + "&" + scope
+
 	engine.CogLog(c, log.Info()).Msgf(
 		"*** Bot ready:  %s#%s", s.State.User.Username, s.State.User.Discriminator)
 	engine.CogLog(c, log.Info()).Msgf(

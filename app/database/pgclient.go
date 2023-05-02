f550156ebde7ae7a11ff95360657a6fab962d383
fiffu <fhejehfif@gmail.com>
2023-05-02 18:52:18 +0800

    pgclient: Only log args if they're given

--- app/database/pgclient.go
+++ app/database/pgclient.go
@@ -46,7 +46,9 @@ func (c *pgclient) Close() error {
 
 func (c *pgclient) Query(query string, args ...interface{}) (IRows, error) {
 	log.Info().Msgf("Query: %s", query)
-	log.Info().Msgf(" Args: %v", args)
+	if len(args) > 0 {
+		log.Info().Msgf(" Args: %v", args)
+	}
 	rows, err := c.pool.Query(query, args...)
 	if err == sql.ErrNoRows {
 		return rows, fmt.Errorf("%w (driver: %v)", ErrNoRecords, err)
@@ -56,7 +58,9 @@ func (c *pgclient) Query(query string, args ...interface{}) (IRows, error) {
 
 func (c *pgclient) Exec(query string, args ...interface{}) (IResult, error) {
 	log.Info().Msgf("Exec: %s", query)
-	log.Info().Msgf("Args: %v", args)
+	if len(args) > 0 {
+		log.Info().Msgf(" Args: %v", args)
+	}
 	affected, err := c.pool.Exec(query, args...)
 	return affected, err
 }

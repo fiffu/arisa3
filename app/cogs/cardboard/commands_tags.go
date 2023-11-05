0fb76a5453b8fed1e6c2629f316e8856bc4ac933
fiffu <fhejehfif@gmail.com>
2023-11-05 09:51:29 +0800

    Add missing command handler for /tags

--- app/cogs/cardboard/commands_tags.go
+++ app/cogs/cardboard/commands_tags.go
@@ -1,6 +1,14 @@
 package cardboard
 
-import "github.com/fiffu/arisa3/app/types"
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/fiffu/arisa3/app/cogs/cardboard/api"
+	"github.com/fiffu/arisa3/app/types"
+	"github.com/fiffu/arisa3/lib/functional"
+)
 
 func (c *Cog) tagSuggestCommand() *types.Command {
 	return types.NewCommand("tags").ForChat().
@@ -9,5 +17,31 @@ func (c *Cog) tagSuggestCommand() *types.Command {
 			types.NewOption(OptionQuery).
 				String().Required(),
 		).
-		Handler(c.promote)
+		Handler(c.tagAutocompleteSuggest)
+}
+
+func (c *Cog) tagAutocompleteSuggest(ctx context.Context, req types.ICommandEvent) error {
+	queryStr, _ := req.Args().String(OptionQuery)
+
+	suggestedTags, err := c.domain.TagsSearch(queryStr)
+	if err != nil {
+		return err
+	}
+
+	var desc string
+	if len(suggestedTags) > 0 {
+		lines := functional.Map(suggestedTags, func(tag *api.TagSuggestion) string {
+			return fmt.Sprintf("%s (%d posts)", tag.Name, tag.PostCount)
+		})
+		strings.Join(lines, "\n")
+	} else {
+		desc = fmt.Sprintf("There's no tags that match '%s'", desc)
+	}
+
+	emb := types.NewEmbed().
+		Colour(embedColour).
+		Title(fmt.Sprintf("Tags matching '%s'", queryStr)).
+		Description(desc)
+	resp := types.NewResponse().Embeds(emb)
+	return req.Respond(ctx, resp)
 }

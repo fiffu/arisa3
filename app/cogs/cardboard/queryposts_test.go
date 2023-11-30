0ebce71c85331f44c87f2fcef7aa81eb64d45b01
fiffu <fhejehfif@gmail.com>
2023-11-30 14:28:01 +0800

    Make WithUnsafe() also include -rating:s

--- app/cogs/cardboard/queryposts_test.go
+++ app/cogs/cardboard/queryposts_test.go
@@ -10,14 +10,13 @@ func Test_Tags_WithMagic_ShouldYieldRatingTags(t *testing.T) {
 	q := NewQuery("xyz").WithMagic()
 
 	q.WithSafe()
+	tags := []string{"xyz"}
 	actual := q.Tags()
-	assert.Contains(t, actual, "xyz")
-	assert.Contains(t, actual, ratingG)
+	assert.Equal(t, append(tags, safeTags...), actual)
 
 	q.WithUnsafe()
 	actual = q.Tags()
-	assert.Contains(t, actual, "xyz")
-	assert.Contains(t, actual, noRatingG)
+	assert.Equal(t, append(tags, unsafeTags...), actual)
 }
 
 func Test_Tags_WithNoMagic_ShouldDropRatingTags(t *testing.T) {
@@ -26,12 +25,12 @@ func Test_Tags_WithNoMagic_ShouldDropRatingTags(t *testing.T) {
 	q.WithSafe()
 	actual := q.Tags()
 	assert.Contains(t, actual, "xyz")
-	assert.NotContains(t, actual, noRatingG)
-	assert.NotContains(t, actual, ratingG)
+	assert.NotContains(t, actual, unsafeTags)
+	assert.NotContains(t, actual, safeTags)
 
 	q.WithUnsafe()
 	actual = q.Tags()
 	assert.Contains(t, actual, "xyz")
-	assert.NotContains(t, actual, noRatingG)
-	assert.NotContains(t, actual, ratingG)
+	assert.NotContains(t, actual, unsafeTags)
+	assert.NotContains(t, actual, safeTags)
 }
